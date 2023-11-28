f4926f3c475838e1f1417adc87743d456875c53f
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-11-28 10:45:50 +0100

    work on part observer

--- index.js
+++ index.js
@@ -1,61 +1,30 @@
-window.addEventListener(
-  "load",
-  function (event) {
-    boxElement = document.querySelector("#elem");
+let options = {
+  root: document.querySelector("#app"),
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+/**
+ * 
+ * @param {boolean} entries 
+ * @param {object} observer 
+ */
 
-    createObserver();
-  },
-  false
-);
-
-function buildThresholdList() {
-  var thresholds = [];
-
-  for (var i = 1.0; i <= numSteps; i++) {
-    var ratio = i / numSteps;
-    thresholds.push(ratio);
-  }
-
-  thresholds.push(0);
-  return thresholds;
-}
-
-function createObserver() {
-  var observer;
-
-  var options = {
-    root: document.querySelector("#app"),
-    rootMargin: "0px",
-    threshold: buildThresholdList(),
-  };
-
-  observer = new IntersectionObserver(handleIntersect, options);
-  observer.observe(boxElement);
-}
-
-var numSteps = 20.0;
-
-var boxElement;
-var prevRatio = 0.0;
-var increasingColor = "";
-var decreasingColor = "white";
-
-// On met l'ensemble en place.
-
-function handleIntersect(entries, observer) {
-  entries.forEach(function (entry) {
-    if (entry.intersectionRatio > prevRatio) {
-      entry.target.style.backgroundColor = increasingColor.replace(
-        "ratio",
-        entry.intersectionRatio
-      );
-    } else {
-      entry.target.style.backgroundColor = decreasingColor.replace(
-        "ratio",
-        entry.intersectionRatio
-      );
+let handleIntersect = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting===true) {
+      //  entry.target.style.color="blue";
+        entry.target.style.translate = "-120px";
     }
-
-    prevRatio = entry.intersectionRatio;
+    else{
+       entry.target.style.translate = "120px";
+    }
+    console.log(observer);
   });
-}
+};
+
+let observer = new IntersectionObserver(handleIntersect, options);
+
+let target = document.querySelector("#elem");
+observer.observe(target);
+
+
